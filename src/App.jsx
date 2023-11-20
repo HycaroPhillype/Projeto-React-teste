868be35bede7361be72c69bb1101ea424cf6b54a
HycaroPhillype <hycarophillype0@gmail.com>
2023-11-20 19:43:55 -0300

    feat: Header
    Criei um Header separado estilizado, para poder usar em outro componentes, sem que afete a mudança em outros

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
+
+import './styles.css';
   
   export function App() {
     return (
       <div>
+        <Header />
+
+
         <Post  
           author="Hycaro Phillype" 
           content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime eaque ducimus soluta atque? Laboriosam saepe porro praesentium minus nihil consectetur amet consequatur, quidem reprehenderit cum quis iste tenetur hic harum!"
