61f837b8c469e5d5545c5b20f107b42f23f3c749
HycaroPhillype <hycarophillype0@gmail.com>
2023-11-21 21:19:13 -0300

    feat: style global
    Adicionando o logo IMG-JSV

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import styles from './Header.module.css'
 
-export function Header () {
+import igniteLogo from '../assets/ignite-logo.svg';
+
+
+export function Header() {
     return (
         <header className={styles.header}>
-            <strong>Hycaro Phillype</strong> 
+            <img src={igniteLogo} alt="Logo tipo do Ignite" />
        </header>
-    )
+    );
 }
