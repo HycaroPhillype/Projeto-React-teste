0fc2f3cd8c3eda21b0c26db2574c7cca2bd02f21
HycaroPhillype <hycarophillype0@gmail.com>
2023-11-22 10:39:20 -0300

    feat: Sidebar
    Stylizando Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
   
+
   export function App() {
     return (
       <div>
         <Header />
 
-
-        <Post  
-          author="Hycaro Phillype" 
-          content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime eaque ducimus soluta atque? Laboriosam saepe porro praesentium minus nihil consectetur amet consequatur, quidem reprehenderit cum quis iste tenetur hic harum!"
-        />
-        <Post
-          author="Satoro Gojo"
-          content="O animal é apelão de mais!"
-        />
+        <div className={styles.wrapper}>
+          
+          <Sidebar />
+          <main>
+            <Post  
+              author="Hycaro Phillype" 
+              content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime eaque ducimus soluta atque? Laboriosam saepe porro praesentium minus nihil consectetur amet consequatur, quidem reprehenderit cum quis iste tenetur hic harum!"
+            />
+            <Post
+              author="Satoro Gojo"
+              content="O animal é apelão de mais!"
+            />
+          </main>
+        </div>
 
       </div>
     )
