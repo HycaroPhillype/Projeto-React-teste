8d999a59736261aa306ee8ddbabc403e9bc10d24
HycaroPhillype <hycarophillype0@gmail.com>
2023-11-27 21:11:07 -0300

    feat: Inteirando no JSX
    Fazendo posts individuais!!

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,47 @@ import styles from './App.module.css';
 
 import './global.css';
 
-  
+// author: { avatar_url: "", name: "", role: ""}
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/HycaroPhillype.png',
+      name: 'Hycaro Phillype',
+      role: 'CTO @ Rocekeseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa'},
+
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀'},
+
+      { type: 'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2023-11-27 20:57:00'),
+
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/HycaroPhillype.png',
+      name: 'Hycaro Phillype',
+      role: 'Educator  @Rocekeseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa'},
+
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀'},
+
+      { type: 'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2023-11-15 20:57:00'),
+    
+  },
+];
 
   export function App() {
     return (
@@ -17,14 +57,15 @@ import './global.css';
           
           <Sidebar />
           <main>
-            <Post  
-              author="Edimara Lopes" 
-              content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime eaque ducimus soluta atque? Laboriosam saepe porro praesentium minus nihil consectetur amet consequatur, quidem reprehenderit cum quis iste tenetur hic harum!"
-            />
-            <Post
-              author="Satoro Gojo"
-              content="O animal é apelão de mais!"
-            />
+            {posts.map(post => {
+              return (
+               <Post
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            })}
           </main>
         </div>
 
