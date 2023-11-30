9ebe19c394e21ebfff9f42c1c611d3d56e437e4f
HycaroPhillype <hycarophillype0@gmail.com>
2023-11-30 13:25:36 -0300

    feat: Estado (useState)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,9 +6,15 @@ import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 
 import styles from './Post.module.css';
+import { useState } from 'react';
 
 
 export function Post({ author, publishedAt, content }) {
+    const [comments, setComments] = useState([
+        1,
+        2,
+     ])
+
     const publiDateForm = format(publishedAt, "d 'de' LLLL 'as' HH:mm'h'", {
         locale: ptBR,
     })
@@ -17,6 +23,14 @@ export function Post({ author, publishedAt, content }) {
         locale: ptBR,
         addSuffix: true,
     })
+
+    function handleCreateNewComment() {
+        event.preventDefault()
+
+        setComments([...comments, comments.length 
+        + 1]);
+        
+    } 
 
     return (
         <article className={styles.post}>
@@ -45,7 +59,7 @@ export function Post({ author, publishedAt, content }) {
               })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
                 <textarea
@@ -58,10 +72,9 @@ export function Post({ author, publishedAt, content }) {
            
 
            <div className={styles.commentList}>
-            <Comment />  
-            <Comment />  
-            <Comment />  
-            <Comment /> 
+            {comments.map(comment => {
+                return <Comment />
+            })}
             </div>
         </article>
     )
